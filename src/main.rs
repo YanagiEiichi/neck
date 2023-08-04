8afa1f7457d3be3bbd05083d47ffd4d6b406b8bc
YanagiEiichi <576398868@qq.com>
2023-08-04 23:35:48 +0800

    Update description

--- src/main.rs
+++ src/main.rs
@@ -16,16 +16,16 @@ struct Args {
 
 #[derive(Subcommand, Debug)]
 enum Commands {
-    /// Start an HTTP proxy server
+    /// Start a Neck HTTP proxy server
     Serve {
-        /// Binding the listeing address, defaults "0.0.0.0:1081"
+        /// Binding the listening address defaults "0.0.0.0:1081"
         addr: Option<String>,
     },
     /// Create some worker connections and join the pool of the server
     Join {
         /// Proxy server address
         addr: String,
-        /// Provided connections, defaults 100
+        /// The provided connections defaults 100
         #[arg(short, long)]
         connections: Option<u16>,
     },

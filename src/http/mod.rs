b608089d7f319434849708b364cf54ce84b156ac
YanagiEiichi <576398868@qq.com>
2023-08-11 00:10:04 +0800

    Extract first_line mod

--- src/http/mod.rs
+++ src/http/mod.rs
@@ -1,11 +1,14 @@
 mod error;
+mod first_line;
 mod headers;
 mod protocol;
 mod request;
 mod response;
+
 mod tests;
 
 pub use error::*;
+pub use first_line::*;
 pub use headers::*;
 pub use protocol::*;
 pub use request::*;
--- src/http/protocol.rs
+++ src/http/protocol.rs
@@ -4,8 +4,7 @@ use tokio::io::{AsyncBufReadExt, AsyncRead, AsyncReadExt, AsyncWrite, AsyncWrite
 
 use tokio::io::BufReader;
 
-use super::error::HttpError;
-use super::Headers;
+use super::{FirstLine, Headers, HttpError};
 
 /// Read a group of lines ending with an empty line from a BufReader.
 async fn read_lines<T>(stream: &mut BufReader<T>) -> Result<Vec<String>, Box<dyn Error>>
@@ -73,65 +72,6 @@ pub trait HttpCommon {
 
     /// Get the payload.
     fn get_payload(&self) -> &Vec<u8>;
-}
-
-#[derive(Debug)]
-pub struct FirstLine(String, usize, usize);
-
-impl FirstLine {
-    /// Creates a new [`FirstLine`].
-    pub fn new(first: &str, second: &str, third: &str) -> Self {
-        FirstLine(
-            format!("{} {} {}", first, second, third),
-            first.len(),
-            first.len() + 1 + second.len(),
-        )
-    }
-
-    /// Parse an HTTP first line.
-    ///
-    /// For example:
-    /// raw = "GET /home HTTP/1.1"
-    /// gap1 = 3 # Location of the first space character.
-    /// offset = gap1 + 1 = 4 # Skip the first space.
-    /// gap2 = offset + 5 = 9 # Find space location from "/home .."
-    /// Therefore,
-    /// get_first returns [..gap1] is "GET"
-    /// get_second returns [gap1+1..gap2] is "/home"
-    /// get_third returns [gap2+1..] is "HTTP/1.1"
-    ///
-    pub fn parse(raw: String) -> Option<Self> {
-        let gap1 = raw.find(' ')?;
-        let offset = gap1 + 1;
-        let gap2 = offset + raw[offset..].find(' ')?;
-        Some(FirstLine(raw, gap1, gap2))
-    }
-
-    /// Returns a reference to the get first of this [`FirstLine`].
-    pub fn get_first(&self) -> &str {
-        &self.0[..self.1]
-    }
-
-    /// Returns a reference to the get second of this [`FirstLine`].
-    pub fn get_second(&self) -> &str {
-        &self.0[self.1 + 1..self.2]
-    }
-
-    /// Returns a reference to the get third of this [`FirstLine`].
-    pub fn get_third(&self) -> &str {
-        &self.0[self.2 + 1..]
-    }
-
-    /// Write all data to an AsyncWrite
-    pub async fn write_to<T>(&self, w: &mut T) -> Result<(), Box<dyn Error>>
-    where
-        T: Unpin,
-        T: AsyncWrite,
-    {
-        w.write_all(self.0.as_bytes()).await?;
-        w.write_all(b"\r\n").await?;
-        Ok(())
-    }
 }
 
 #[derive(Debug)]
